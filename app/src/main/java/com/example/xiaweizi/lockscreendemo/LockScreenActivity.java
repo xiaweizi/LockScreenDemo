45a46300b02ae67034fc0deccbda80a736428b9e
xiaweizi <1012126908@qq.com>
2018-10-07 09:44:28 +0800

    [dev] 展示在系统锁屏之上

--- app/src/main/java/com/example/xiaweizi/lockscreendemo/LockScreenActivity.java
+++ app/src/main/java/com/example/xiaweizi/lockscreendemo/LockScreenActivity.java
@@ -2,6 +2,7 @@ package com.example.xiaweizi.lockscreendemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class LockScreenActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class LockScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock_screen);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
     }
 }
