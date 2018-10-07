4fb3535825d3555d09660a07bb6195d3dc5baa00
xiaweizi <1012126908@qq.com>
2018-10-07 09:38:12 +0800

    [dev] 添加锁屏展示测试代码

---
+++ app/src/main/java/com/example/xiaweizi/lockscreendemo/LockScreenActivity.java
@@ -0,0 +1,13 @@
+package com.example.xiaweizi.lockscreendemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LockScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock_screen);
+    }
+}
---
+++ app/src/main/java/com/example/xiaweizi/lockscreendemo/LockScreenService.java
@@ -0,0 +1,59 @@
+package com.example.xiaweizi.lockscreendemo;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.text.TextUtils;
+import android.util.Log;
+
+public class LockScreenService extends Service {
+    private static final String TAG = "LockScreen::";
+    private LockScreenBroadcastReceiver receiver;
+
+    public LockScreenService() {
+        Log.i(TAG, "LockScreenService: 构造函数");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "onCreate: 服务已开启");
+        receiver = new LockScreenBroadcastReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    class LockScreenBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (TextUtils.equals(intent.getAction(), Intent.ACTION_SCREEN_OFF)) {
+                Log.i(TAG, "onReceive: 收到锁屏广播");
+                Intent lockScreen = new Intent(LockScreenService.this, LockScreenActivity.class);
+                lockScreen.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(lockScreen);
+            }
+        }
+    }
+}
